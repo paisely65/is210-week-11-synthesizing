0abcd8593418f573b0631dab88d052e3f9f22e65
paisely65 <cunysps@gmail.com>
2016-04-08 01:07:18 -0400

    task 1.

---
+++ chessmaster.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Starter data module"""
+
+import time
+
+
+class ChessPiece(position):
+    """ A class that shows the posistion of gamepieses."""
+    Attributes:
+        position(string): stores the title notation of its current position
+        moves(list): stores tuples of information
+        
+
+def class algebraic_to_numeric(title):
+    """This class returns a tuple."""
+    Arguments:
+        title(tuple): Plae values for the game.
+        
+    Returns (tuples): A list of tuple with two values
+
+    
+    def is_legal_move():
+        """This functions tests whether a move is legal.
